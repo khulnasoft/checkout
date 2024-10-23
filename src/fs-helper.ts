23bbb04421270471e3345c564a88af5b343102b0
fortishield <161459699+FortiShield@users.noreply.github.com>
2024-10-24 00:27:13 +0600

    Update fs-helper.ts
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>

--- src/fs-helper.ts
+++ src/fs-helper.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs'
+import * as path from 'path'
 
 export function directoryExistsSync(path: string, required?: boolean): boolean {
   if (!path) {
@@ -80,4 +81,26 @@ export function fileExistsSync(path: string): boolean {
   }
 
   return false
+}
+
+/**
+ * Searches a given directory and returns a list of file paths giving all files in that directory.
+ * The file paths all begin at `dir`.
+ *
+ * @param dir The directory to search
+ * @returns A list of file paths,
+ */
+export async function readdirRecursive(dir: string): Promise<string[]> {
+  const files = await fs.promises.readdir(dir);
+  const result: string[] = [];
+  for (const file of files) {
+    const filePath = path.join(dir, file);
+    const stat = await fs.promises.stat(filePath);
+    if (stat.isDirectory()) {
+      result.push(...(await readdirRecursive(filePath)));
+    } else {
+      result.push(filePath);
+    }
+  }
+  return result;
 }
